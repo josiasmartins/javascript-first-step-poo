9b077d5706b0ee033745b997fdc2c456ccb7e43f
Caitano <josiasmartinscaitano@gmail.com>
2022-05-13 10:15:56 -0300

    feat: adicoando o métodos acessores: Getters

--- User.js
+++ User.js
@@ -12,15 +12,37 @@ export default class User {
         this.#ativo = ativo;
     }
 
-    #montaObjeto() {
-        return ({
-            nome: this.#nome,
-            email: this.#email,
-            nascimento: this.#nascimento,
-            role: this.#role,
-            ativo: this.#ativo
-        })
+    // função get: não aceita argumentos. Chama como se fosse uma propriedade
+    // get: aceita apenas leituras
+    get nome() {
+        return this.#nome
     }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    // #montaObjeto() {
+    //     return ({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
 
     exibirInfos() {
         const objUser = this.#montaObjeto();
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import Docente from './Docente.js';
 import Admin from './Admin.js';
 
 const novoUser = new User('Mariana', 'm@m.com', '1999-01-01');
-console.log(novoUser.exibirInfos());
+// console.log(novoUser.exibirInfos());
 
 const novoAdmin = new Admin('Ibag', 'r@r.com', '2021-01-01');
-console.log(novoAdmin.exibirInfos());
-console.log(novoAdmin.nomeAdmin());
+console.log(novoAdmin.nome)
